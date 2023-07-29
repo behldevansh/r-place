4304b3b9781d7e21e1ea1ee2e4b23cd76052dbcf
behldevansh <heydevanshbehl@gmail.com>
2023-07-29 17:25:21 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+let container=document.querySelector('.container')
+let gridButton=document.querySelector('.submit-grid')
+let clearGridButton=document.querySelector('.clear-grid')
+let gridWidth=document.getElementById
